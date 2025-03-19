b334d8ba4d5f45225159d9f1504455cbb5baa443
Christian-Santiago0710 <>
2025-03-19 12:43:16 +0800

    Last Sync: 2025-03-19 04:43:16 (Mobile)

---
+++ newfile.java
@@ -0,0 +1,9 @@
+public class CarInfo {
+    public static void main(String[] args) {
+        String carMake = "Toyota";
+        String carModel = "Corolla";
+        int carYear = 2020;
+
+        System.out.println("Car: " + carYear + " " + carMake + " " + carModel);
+    }
+}
